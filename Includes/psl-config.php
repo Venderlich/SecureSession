f31d249e99a92906c88c92acc0b51e61235830bc
Venderlich <tristan.cuenot@laposte.net>
2018-03-04 15:58:09 +0100

    second commit

---
+++ Includes/psl-config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Voici les détails de connexion à la base de données
+ */
+define("HOST", "localhost");     // L’hébergeur où vous voulez vous connecter.
+define("USER", "sec_user");    // Le nom d’utilisateur de la base de données.
+define("PASSWORD", "4Fa98xkHVd2XmnfK");    // Le mot de passe de la base de données.
+define("DATABASE", "secure_login");    // Le nom de la base de données.
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+
+define("SECURE", FALSE);    // A Passer a true plus tard
